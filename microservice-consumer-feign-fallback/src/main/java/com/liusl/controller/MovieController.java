9d257e9bbe23c9c210c1796cc6d4f211694810ad
liushunli <liusl6@yusys.com.cn>
2017-09-13 23:02:48 +0800

    修复在Feign中无法开启Hystrix的问题

--- microservice-consumer-feign-fallback/src/main/java/com/liusl/controller/MovieController.java
+++ microservice-consumer-feign-fallback/src/main/java/com/liusl/controller/MovieController.java
@@ -2,19 +2,14 @@ package com.liusl.controller;
 
 import com.liusl.entity.User;
 import com.liusl.feign.UserFeignClient;
-import com.netflix.discovery.converters.Auto;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 
 /**
